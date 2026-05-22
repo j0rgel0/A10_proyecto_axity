2c7c9133103d6c1c33baaf207ebf0539e12fb91f
Jorge López <jorge.lopezvz@udlap.mx>
2026-05-22 00:55:15 -0600

    inicializa Spring Boot con Java diecisiete y Maven

---
+++ src/main/java/com/parque/dinosaurios/ParqueDinosauriosApplication.java
@@ -0,0 +1,13 @@
+package com.parque.dinosaurios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueDinosauriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueDinosauriosApplication.class, args);
+	}
+
+}
